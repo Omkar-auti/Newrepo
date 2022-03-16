dbcfc53ce7568b17503a0d1cbfb6a33030860af6
Omkar-auti <101172766+Omkar-auti@users.noreply.github.com>
2022-03-16 10:23:37 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String ar[])
+{ System.out.println("Hwllo 1");
+ System.out.println("Hwllo 2");
+System.out.println("Hwllo 3");
+System.out.println("Hwllo 4");
+}
+}

c977dcc5709a9cbceb826a307f7f6dbd9b053259
Omkar-auti <101172766+Omkar-auti@users.noreply.github.com>
2022-03-20 23:09:34 +0530

    Add files via upload

---
+++ Sub.java
@@ -0,0 +1,15 @@
+class Sub{
+	static int sub(int a,int b)//function defination 
+	{
+		int result=a-b;
+		return result;
+	}
+
+
+
+
+ public static void main(String [] args)
+ {
+      	System.out.println(sub(55,33));// Function call 
+}
+}
